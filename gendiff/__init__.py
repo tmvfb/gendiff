4eb526a0c668211e6a3167218371f46410a00a07
tmvfb <darestade@gmail.com>
2023-01-12 00:22:50 +0300

    add prototype of generate_diff

---
+++ gendiff/__init__.py
@@ -0,0 +1,2 @@
+# __all__ = ('generate_diff', 'add_cli')
+from .gendiff import generate_diff, add_cli  # noqa
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,24 +1,10 @@
 #! usr/bin/env/python3
-import argparse
-
-
-def show_help():
-    parser = argparse.\
-             ArgumentParser(
-                description='''Compares two configuration
-                files and shows a difference.'''
-             )
-    parser.add_argument('file1', metavar='first_file',
-                        type=argparse.FileType('r'))
-    parser.add_argument('file2', metavar='second_file',
-                        type=argparse.FileType('r'))
-    parser.add_argument('-f', '--format', metavar='FORMAT',
-                        help='set format of output')
-    parser.parse_args()
+from gendiff import add_cli, generate_diff
 
 
 def main():
-    show_help()
+    add_cli()
+    generate_diff()
 
 
 if __name__ == '__main__':
