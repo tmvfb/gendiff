bafac289c1494df41631b0342a95ec545107b8b4
tmvfb <darestade@gmail.com>
2023-01-17 22:14:55 +0300

    refactor files_open module

--- gendiff/modules/files_open.py
+++ gendiff/modules/files_open.py
@@ -7,13 +7,11 @@ def files_open(filepath):
     def loader(path):
         if filepath.endswith('.json'):
             return json.load(path)
-        if filepath.endswith('.yaml') or filepath.endswith('.yml'):
+        if filepath.endswith(('.yaml', '.yml')):
             return yaml.load(path, Loader=yaml.SafeLoader)
     try:
         with open('filepath') as working_file:
-            working_file = loader(working_file)
+            return loader(working_file)
     except FileNotFoundError:
         with open('/'.join([getcwd(), filepath])) as working_file:
-            working_file = loader(working_file)
-
-    return working_file
+            return loader(working_file)
