46bd65fb9590d9606d7516d8512b00ac480cce45
tmvfb <darestade@gmail.com>
2023-01-26 18:25:31 +0300

    add recursive diff for nested json files

--- gendiff/modules/diff.py
+++ gendiff/modules/diff.py
@@ -15,16 +15,16 @@ def tree_diff(dict1, dict2):  # returns a generator (of nested dicts)
 
     for key, value in dict2.items():
         if key not in dict1.keys():  # checks if key in 1st dict
-            yield {''.join(['- ', key]): value}  # returns dict element
+            yield {''.join(['+ ', key]): value}  # returns dict element
 
     for key, value in dict1.items():
         if key not in dict2.keys():  # checks if key in 2nd dict
-            yield {''.join(['+ ', key]): value}
+            yield {''.join(['- ', key]): value}
 
         # checks if at least one value (child) is not dict
         # if true, no need to go deeper (no nested dict)
         elif not all(isinstance(i, dict) for i in [value, dict2[key]]):
-            yield compare({key: value}, {key: dict2[key]})
+            yield dict(compare({key: value}, {key: dict2[key]}))
 
 # if key in both dicts + can go deeper (=values are dicts): go deep recursively
 # child is a dict in this case, use dict comprehension to define it correctly
@@ -41,7 +41,7 @@ def tree_diff(dict1, dict2):  # returns a generator (of nested dicts)
 def compare(pair1, pair2):
     for key, value in pair1.items():
         if pair1 == pair2:
-            return {k: v for k, v in pair1.items()}
+            yield (key, value)
         else:
-            return {''.join(['- ', key]): value}
-            return {''.join(['+ ', key]): pair2[key]}
+            yield (''.join(['- ', key]), value)
+            yield (''.join(['+ ', key]), pair2[key])
--- gendiff/modules/return_json.py
+++ gendiff/modules/return_json.py
@@ -3,9 +3,39 @@ from gendiff.modules.diff import diff
 
 def return_json(file1, file2):
     result = diff(file1, file2)
-    return result
-    # result.sort(key=lambda x: x[2])
-    # result = '\n  '.join(result).\
-    #     replace('True', 'true').\
-    #     replace('False', 'false')
-    # return ''.join(("{\n  ", result, "\n}\n"))
+    result = make_stylish(result)
+    # last symbol of result is '\n', don't need it
+    return '\n'.join(['{', result[:-1], '}'])\
+               .replace('False', 'false')\
+               .replace('True', 'true')\
+               .replace('None', 'null')
+
+
+def make_stylish(node, memory='', indent='    '):
+    node = dict(sorted(node.items(), key=custom_sort))
+
+    for key, value in node.items():
+        key_indent = indent_checker(key, indent)
+        memory += ''.join([key_indent, str(key), ': '])
+        if not isinstance(value, dict):
+            memory += ''.join([str(value), '\n'])
+        else:
+            memory += '{\n'
+            memory = make_stylish(value, memory, indent + '    ')
+            memory += ''.join([indent, '}', '\n'])
+    return memory
+
+
+def custom_sort(items):
+    if items[0][0] in ['-', '+']:
+        return items[0][2:]
+    else:
+        return items[0]
+
+
+def indent_checker(dict_key, indent):
+    if str(dict_key)[0] in ['-', '+']:
+        key_indent = indent[2:]
+    else:
+        key_indent = indent
+    return key_indent
